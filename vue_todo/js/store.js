26369f9dd41f309f25f853da86a661452d94cb1a
Jean Jordaan <jean.jordaan@gmail.com>
2017-11-07 23:25:11 +0700

    Fumbling towards AJAX

--- vue_todo/js/store.js
+++ vue_todo/js/store.js
@@ -2,17 +2,30 @@
 
 (function (exports) {
 
-	'use strict';
+    'use strict';
 
-	var STORAGE_KEY = 'todos-vuejs';
+    var STORAGE_KEY = 'todos-vuejs';
 
-	exports.todoStorage = {
-		fetch: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-		},
-		save: function (todos) {
-			localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-		}
-	};
+    exports.todoStorage = {
+        fetch: function () {
+            return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+        },
+        save: function (todos) {
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+
+            // START
+            todos.forEach(function (todo) {
+                this.$http.post('db/todo', {
+                    '@type': 'Todo',
+                    text: todo.title,
+                    completed: todo.completed
+                }, function (data, status, request) {
+                    //this.people.push(data);
+                    //
+                   });
+                })();
+            // END
+        }
+    };
 
 })(window);
